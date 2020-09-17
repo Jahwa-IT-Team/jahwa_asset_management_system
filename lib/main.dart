f009fde93397c8158950b74bfa5b28ea63a6e02e
Jahwa-IT-Team <computer@jahwa.co.kr>
2020-09-17 17:29:33 +0900

    master

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,34 @@ import 'provider/facility_trade_receive_repository.dart';
 import "provider/user_repository.dart";
 import 'util/localization/language_constants.dart';
 
-
 //void main() => runApp(MyApp());
 void main() {
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.dumpErrorToConsole(details);
-    if (kReleaseMode)
-      exit(1);
+    if (kReleaseMode) exit(1);
   };
   //runApp(MyApp());
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => UserRepository(),),
-        ChangeNotifierProvider(create: (_) => FacilityTradeCommonRepository(),),
-        ChangeNotifierProvider(create: (_) => FacilityTradeRequestRepository(),),
-        ChangeNotifierProvider(create: (_) => FacilityTradeSendRepository(),),
-        ChangeNotifierProvider(create: (_) => FacilityTradeReceiveRepository(),),
-        ChangeNotifierProvider(create: (_) => FacilityLocationRepository(),),
+        ChangeNotifierProvider(
+          create: (_) => UserRepository(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FacilityTradeCommonRepository(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FacilityTradeRequestRepository(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FacilityTradeSendRepository(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FacilityTradeReceiveRepository(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FacilityLocationRepository(),
+        ),
       ],
       child: MyApp(),
     ),
@@ -40,6 +50,7 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
+
   static void setLocale(BuildContext context, Locale newLocale) {
     _MyAppState state = context.findAncestorStateOfType<_MyAppState>();
     state.setLocale(newLocale);
@@ -73,28 +84,29 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-   
-    FacilityTradeCommonRepository $facilityTradeCommonRepository = Provider.of<FacilityTradeCommonRepository>(context, listen:true);
-    if(!$facilityTradeCommonRepository.firstInit){
+    FacilityTradeCommonRepository $facilityTradeCommonRepository =
+        Provider.of<FacilityTradeCommonRepository>(context, listen: true);
+    if (!$facilityTradeCommonRepository.firstInit) {
       $facilityTradeCommonRepository.init();
     }
-    
-    if(this.checkAutoLogin == false){
-      UserRepository $userRepository = Provider.of<UserRepository>(context, listen: false);
-        $userRepository.autoLogin().then((value){
-          print("Auto Check $value");
-          if(value){
-            initialRoute = homeRoute;
-          }else{
-            initialRoute = loginRoute;
-          }
-          print("InitalRoute1 : $initialRoute");
-          
-          setState(() {
-            this.checkLoginInfo = true;
-          });
+
+    if (this.checkAutoLogin == false) {
+      UserRepository $userRepository =
+          Provider.of<UserRepository>(context, listen: false);
+      $userRepository.autoLogin().then((value) {
+        print("Auto Check $value");
+        if (value) {
+          initialRoute = homeRoute;
+        } else {
+          initialRoute = loginRoute;
+        }
+        print("InitalRoute1 : $initialRoute");
+
+        setState(() {
+          this.checkLoginInfo = true;
         });
-        checkAutoLogin = true;
+      });
+      checkAutoLogin = true;
     }
     if (this._locale == null || this.checkLoginInfo == false) {
       return Container(
@@ -107,10 +119,12 @@ class _MyAppState extends State<MyApp> {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Jahwa Asset Management",
-        theme: ThemeData(primarySwatch: Colors.green, textTheme: TextTheme(headline6: TextStyle(color: Colors.white))),
+        theme: ThemeData(
+            primarySwatch: Colors.green,
+            textTheme: TextTheme(headline6: TextStyle(color: Colors.white))),
         locale: _locale,
         supportedLocales: [
-          Locale("ko",""),
+          Locale("ko", ""),
           Locale("vi", ""),
         ],
         localizationsDelegates: [
@@ -121,8 +135,8 @@ class _MyAppState extends State<MyApp> {
         ],
         localeResolutionCallback: (locale, supportedLocales) {
           if (locale == null) {
-                debugPrint("*language locale is null!!!");
-                    return supportedLocales.first;
+            debugPrint("*language locale is null!!!");
+            return supportedLocales.first;
           }
           for (var supportedLocale in supportedLocales) {
             if (supportedLocale.languageCode == locale.languageCode &&
@@ -134,7 +148,6 @@ class _MyAppState extends State<MyApp> {
         },
         onGenerateRoute: CustomRouter.generatedRoute,
         initialRoute: initialRoute, // loginRoute,
-        
       );
     }
   }
